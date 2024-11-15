cb7dabfd1cfd051419289eb68606bb0aad5cdd1f
zavame <157161880+zavame@users.noreply.github.com>
2024-11-14 20:57:00 -0500

    Delete CSC325_GroupProject/src/main directory

--- CSC325_GroupProject/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.csc325_groupproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.csc325_groupproject to javafx.fxml;
-    exports com.example.csc325_groupproject;
-}
